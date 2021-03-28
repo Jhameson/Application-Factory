454894c347a26d85856fdbccef15c73d78a1ab1a
jhameson <jhamesonlucas22@gmail.com>
2021-03-27 23:08:37 -0300

    inicio modulo 2

--- 1_modulo/script.js
+++ 1_modulo/script.js
@@ -1,4 +1,6 @@
-
+/*
+    TÓPICO: Modificando HTML
+*/
 function btnEntrar(){
     let area = document.getElementById('area');
     let texto = prompt('Qual seu nome?');
@@ -18,21 +20,86 @@ function btnEntrar(){
 let lista = ["APS","Redes", "Autômatos", "Programação Funcional", "CG"];
 lista.length;
 //5
+
 lista [3];
 //Programação Funcional
+
 lista.indexOf("CG");
 //4 //retorna o índice
 //caso não exista, retorna "-1"
-lista.join(",");
+
+lista.join(","); //especifica um separador
 //"APS,Redes, Autômatos, Programação Funcional, CG"
+
 lista.join(" / ")
-"APS / Redes / Autômatos /  Programação Funcional / CG"
+//"APS / Redes / Autômatos /  Programação Funcional / CG"
+
 lista.pop();
 //remove o último
+
 lista.shift();
-//remove o primeiro
+//remove o primeiro elemento do array
+
 lista[4] = "Computação Gráfica";
 //Altera o valor do index passado
+
 lista.push("Projeto Integrador");
-//adiciona no fim da lista
+//adiciona um novo elemento ao final de um array.
+lista.push("Teoria","Projeto 3");
+
+lista.unshift("Básica"); //incluir um novo elemento no início do array
+
+let frutas = ["jaca","banana","maça","uva"]; 
+let frase = frutas.toString(); //array em string
+//"jaca,banana,maça,uva"
+
+//O método concat() cria um novo array mesclando arrays existentes:
+let meninos = ["Lucas","Lucas2"];
+let meninas = ["Bia","Bia2"];
+let mn = meninos.concat(meninas);
+
+/*
+    TÓPICO: Aprendendo Loops
+*/
+
+//while = enquanto
+document.write("While </br>");
+let x = 0;
+while(x < 10){
+    document.write("valor de x: "+x+"</br>");
+    //console.log("valor de x: "+x);
+    x++;
+}
+document.write("</br></br>");
+document.write("For </br>");
+let i;
+for(i = 0; i <x; i++){
+    document.write("valor de i: "+i+"</br>");
+}
+
+//for = até
+
+/*
+    TÓPICO: switchs
+*/
+
+
+
+function pedirValor(){
+    let y = prompt("Informe um valor para y: ");
+    switch(y){
+        case "0":
+            alert("O y vale 0");
+            break;
+        case "1":
+            alert("O y vale 1");
+            break;
+        case "2":
+            alert("O y vale 2");
+            break;
+        default:
+            alert("escolheu errado");
+        
+    }
+}
 
---
+++ 2_modulo/script.js
@@ -0,0 +1,41 @@
+/*
+    TÓPICO: Eventos do mouse
+*/
+
+//onmousedown => evento apertar o botão do mouse
+function apertar(){
+    console.log("Apertou!")
+}
+
+//onmouseup => evento soltar o botão do mouse
+function soltar(){
+    console.log("Soltou!")
+}
+
+//onmousemove => mexendo o mouse em cima do botão
+function movendo(){
+    console.log("Mexendo")
+}
+
+//onmouseover => passou por cima do botao
+function passou(){
+    console.log("Está por cima")
+}
+
+//onmouseout => tirou o mouse de cima do botão
+function tirou(){
+    console.log("Saiu de cima")
+}
+
+//onclick => clicou com o mouse
+function clicou(){
+    console.log("Click")
+}
+
+//oncontextmenu => clicou com o direito
+function mouseDireito(){
+    console.log("Mouse Direito")
+}
+
+//para bloquear a ação de um click bastar colocar "return false" e ajustar o html para esperar um retorno
+
